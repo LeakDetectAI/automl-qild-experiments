ccac9b5d7d62f8d2a0658b1c5160ac06b540ac2d
prithagupta <prithag@mail.uni-paderborn.de>
2023-06-05 22:33:06 +0200

    update the marking the finished job

--- cluster_scripts/exp_ild_batch.sh
+++ cluster_scripts/exp_ild_batch.sh
@@ -3,5 +3,4 @@ for i in $(seq $count);do
     sbatch scripts/run_ild.sh 'leakage_detection'
     sbatch scripts/run_ild_largemem.sh 'leakage_detection'
     sbatch scripts/run_ild_gpu.sh 'leakage_detection'
-
 done
