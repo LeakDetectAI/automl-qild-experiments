91f83e93175803a7cf9a10179b7d5187069674ee
prithagupta <prithag@mail.uni-paderborn.de>
2023-08-14 14:23:58 +0200

    Removed copy script files

--- copy_to_wuppertal.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rsync -avz -P --rsh="ssh -o StrictHostKeyChecking=no -l pritha" --exclude=".git" --exclude="build"  --exclude=".ipynb_checkpoints" --exclude="pycild.egg-info" \
---exclude=".~lock." --exclude=".idea" --exclude="*.pyc" --exclude="*.gitignore" --exclude="experiments/leakage_detection/" \
---exclude="wandb/" ~/git/information-leakage-techniques sca:/home/pritha/
