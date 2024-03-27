25a9a142991815f69c009189e594764da9325719
prithagupta <prithag@mail.uni-paderborn.de>
2024-03-27 21:14:00 +0100

    update

--- cluster_scripts/scripts/run_ild_l_gpu.sh
+++ cluster_scripts/scripts/run_ild_l_gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J "LM_ILDGPU"
+#SBATCH -J "ILD_LM_GPU"
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH --cpus-per-task=32
--- cluster_scripts/scripts/run_ild_n_gpu.sh
+++ cluster_scripts/scripts/run_ild_n_gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J "N_ILDGPU"
+#SBATCH -J "ILD_N_GPU"
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH --cpus-per-task=32
