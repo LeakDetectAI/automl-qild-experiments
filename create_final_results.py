dfb9a8e56ea9806e0c7f65403bbc7da1890589c3
prithagupta <prithag@mail.uni-paderborn.de>
2024-09-24 21:39:05 +0200

    updated the results script

--- create_final_results.py
+++ create_final_results.py
@@ -47,7 +47,8 @@ if __name__ == "__main__":
     db_connector.init_connection()
     result_table = f"results.{schema}"
     avail_jobs = f"{schema}.avail_jobs"
-    final_result_table = f"results.{schema}_full"
+    table_name = f"{schema}_full"
+    final_result_table = f"results.{table_name}"
     select_job = f"""SELECT * FROM {result_table} JOIN {avail_jobs} ON {result_table}.job_id = {avail_jobs}.job_id 
                      WHERE {result_table}.job_id % 20 = {bucket_id} order by {result_table}.job_id;"""
     db_connector.cursor_db.execute(select_job)
@@ -105,7 +106,7 @@ if __name__ == "__main__":
         column_names_query = f"SELECT * FROM {final_result_table} LIMIT 0;"
         db_connector.cursor_db.execute(column_names_query)
         column_names = [desc[0] for desc in db_connector.cursor_db.description]
-        primary_key = f"ALTER TABLE {final_result_table} ADD CONSTRAINT {final_result_table}_final_pkey " \
+        primary_key = f"ALTER TABLE {final_result_table} ADD CONSTRAINT {table_name}_final_pkey " \
                       f"PRIMARY KEY(job_id, n_hypothesis_threshold);"
         db_connector.cursor_db.execute(primary_key)
         logger.info("Primary key constraint added successfully")
